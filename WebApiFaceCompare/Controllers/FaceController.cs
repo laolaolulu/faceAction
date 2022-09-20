b92e8c765556acd4b9a33640ce8892b5cb2465c6
LP-home <1570800510@qq.com>
2022-09-20 19:33:49 +0800

    webapi合并

---
+++ WebApiFaceCompare/Controllers/FaceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenCvSharp;
+
+namespace WebApiFaceCompare.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FaceController : ControllerBase
+    {
+        [HttpPost]
+        public object Compare(string imgUrl, string imgBase64)
+        {
+            using var cap = new VideoCapture(imgUrl);
+            using var img1 = cap.RetrieveMat();
+           // using var img2 =Cv2.ImDecode();
+            return 0;
+        }
+    }
+}
