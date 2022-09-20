0dadef3d15e8b54d17e3567e63ec8b39f7fd3dba
LP <1570800510@qq.com>
2022-09-20 15:15:41 +0800

    Face recognition

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,28 +34,33 @@
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.FaceMatchStatus.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.img)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // FaceMatchStatus
             // 
             this.FaceMatchStatus.Controls.Add(this.img);
-            this.FaceMatchStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.FaceMatchStatus.Location = new System.Drawing.Point(0, 89);
+            this.FaceMatchStatus.Location = new System.Drawing.Point(376, 12);
             this.FaceMatchStatus.Name = "FaceMatchStatus";
-            this.FaceMatchStatus.Size = new System.Drawing.Size(705, 448);
+            this.FaceMatchStatus.Size = new System.Drawing.Size(317, 211);
             this.FaceMatchStatus.TabIndex = 0;
             this.FaceMatchStatus.TabStop = false;
             this.FaceMatchStatus.Text = "Save Path \"./Resource\"";
-            this.FaceMatchStatus.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // img
             // 
             this.img.Dock = System.Windows.Forms.DockStyle.Fill;
             this.img.Location = new System.Drawing.Point(3, 23);
             this.img.Name = "img";
-            this.img.Size = new System.Drawing.Size(699, 422);
+            this.img.Size = new System.Drawing.Size(311, 185);
+            this.img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.img.TabIndex = 1;
             this.img.TabStop = false;
             // 
@@ -67,7 +72,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(27, 35);
+            this.button1.Location = new System.Drawing.Point(12, 23);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 1;
@@ -77,7 +82,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(246, 35);
+            this.button2.Location = new System.Drawing.Point(12, 95);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 29);
             this.button2.TabIndex = 2;
@@ -85,11 +90,54 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Select";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(376, 229);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 211);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selected photos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(311, 185);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "compare";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(705, 537);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.FaceMatchStatus);
@@ -98,6 +146,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.FaceMatchStatus.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.img)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -109,5 +159,9 @@
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,18 +1,11 @@
 ﻿using DlibDotNet;
+using DlibDotNet.Dnn;
 using OpenCvSharp;
-using OpenCvSharp.Flann;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinFormsBlink
@@ -23,12 +16,17 @@ namespace WinFormsBlink
         {
             InitializeComponent();
         }
-
+        ShapePredictor sp;
+        LossMetric net;
+        FrontalFaceDetector detector;
         private void Form1_Load(object sender, EventArgs e)
         {
+            // 加载人脸68特征点检测模型
+            sp = ShapePredictor.Deserialize("Resource/shape_predictor_68_face_landmarks.dat");
 
+            net = LossMetric.Deserialize("Resource/dlib_face_recognition_resnet_model_v1.dat");
 
-
+            detector = Dlib.GetFrontalFaceDetector();
         }
 
         static double eyeAspectRatio(Vector2[] eye)
@@ -46,13 +44,12 @@ namespace WinFormsBlink
             return ear;
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
 
-        }
         bool isread = false;
         private void button1_Click(object sender, EventArgs e)
         {
+            FullObjectDetection shape = null;
+            //  faceDescriptors = null;
             isread = true;
             // define two constants, one for the eye aspect ratio to indicate
             // blink and then a second constant for the number of consecutive
@@ -63,92 +60,131 @@ namespace WinFormsBlink
             var COUNTER = 0;
             var TOTAL = 0;
 
-            VideoCapture cap = new VideoCapture(0);
-            using (var detector = Dlib.GetFrontalFaceDetector())
-            // 加载人脸68特征点检测模型
-            using (var sp = ShapePredictor.Deserialize("Resource/shape_predictor_68_face_landmarks.dat"))
-                while (isread)
+            using VideoCapture cap = new VideoCapture(0);
+
+            while (isread)
+            {
+                using var frameMat = cap.RetrieveMat();
+                var saveimg = frameMat.Clone();
+                byte[] array = new byte[frameMat.Width * frameMat.Height * frameMat.ElemSize()];
+                Marshal.Copy(frameMat.Data, array, 0, array.Length);
+
+                using var cimg = Dlib.LoadImageData<RgbPixel>(array, (uint)frameMat.Height, (uint)frameMat.Width, (uint)(frameMat.Width * frameMat.ElemSize()));
+                var dets = detector.Operator(cimg);
+
+                foreach (var face in dets)
                 {
-                    using var frameMat = cap.RetrieveMat();
-                    var saveimg = frameMat.Clone();
-                    byte[] array = new byte[frameMat.Width * frameMat.Height * frameMat.ElemSize()];
-                    Marshal.Copy(frameMat.Data, array, 0, array.Length);
+                    shape = sp.Detect(cimg, face);
 
-                    using var cimg = Dlib.LoadImageData<RgbPixel>(array, (uint)frameMat.Height, (uint)frameMat.Width, (uint)(frameMat.Width * frameMat.ElemSize()));
-                    var dets = detector.Operator(cimg);
+                    // 画出检测到的脸的矩形框
+                    frameMat.Rectangle(new Rect(face.Left, face.Top, (int)face.Width, (int)face.Height), new Scalar(255, 0, 0));
 
-                    foreach (var rect in dets)
+                    var eye1 = new Vector2[6];
+                    var eye2 = new Vector2[6];
+                    for (uint i = 0; i < shape.Parts; i++)
                     {
-                        // 画出检测到的脸的矩形框
-                        frameMat.Rectangle(new Rect(rect.Left, rect.Top, (int)rect.Width, (int)rect.Height), new Scalar(255, 0, 0));
-                        var shape = sp.Detect(cimg, rect);
-                        var eye1 = new Vector2[6];
-                        var eye2 = new Vector2[6];
-                        for (uint i = 0; i < shape.Parts; i++)
+                        var point = shape.GetPart(i);
+                        // 获取第一只眼睛
+                        if (i >= 36 && i < 42)
                         {
-                            var point = shape.GetPart(i);
-                            // 获取第一只眼睛
-                            if (i >= 36 && i < 42)
-                            {
-                                eye1[i - 36] = new Vector2(point.X, point.Y);
-                            }
-                            // 获取第二只眼睛
-                            if (i >= 42 && i < 48)
-                            {
-                                eye2[i - 42] = new Vector2(point.X, point.Y);
-                            }
-
-                            // 画出检测到的脸的68特征点
-                            frameMat.Circle(point.X, point.Y, 1, new Scalar(0, 255, 0));
-                            //  frameMat.PutText(i.ToString(), new Point(point.X, point.Y), HersheyFonts.HersheySimplex, 0.3, new Scalar(0, 255, 0));
+                            eye1[i - 36] = new Vector2(point.X, point.Y);
+                        }
+                        // 获取第二只眼睛
+                        if (i >= 42 && i < 48)
+                        {
+                            eye2[i - 42] = new Vector2(point.X, point.Y);
                         }
 
-                        var EAR1 = eyeAspectRatio(eye1);
-                        var EAR2 = eyeAspectRatio(eye2);
-                        // average the eye aspect ratio together for both eyes
-                        var ear = (EAR1 + EAR2) / 2.0;
-                        frameMat.PutText(string.Format("EAR: {0:N2}", ear), new OpenCvSharp.Point(300, 30), HersheyFonts.HersheySimplex, 0.7, new Scalar(0, 0, 255), 2);
-                        // check to see if the eye aspect ratio is below the blink
-                        // threshold, and if so, increment the blink frame counter
-                        if (ear < EYE_AR_THRESH)
-                        {
-                            COUNTER += 1;
-                            Debug.WriteLine(ear);
-                        }
-                        // otherwise, the eye aspect ratio is not below the blink
-                        // threshold
-                        else
-                        {
-                            //if the eyes were closed for a sufficient number of
-                            // then increment the total number of blinks
-                            if (COUNTER >= EYE_AR_CONSEC_FRAMES)
-                                TOTAL += 1;
-                            // reset the eye frame counter
-                            COUNTER = 0;
-                        }
+                        // 画出检测到的脸的68特征点
+                        frameMat.Circle(point.X, point.Y, 1, new Scalar(0, 255, 0));
+                        //  frameMat.PutText(i.ToString(), new Point(point.X, point.Y), HersheyFonts.HersheySimplex, 0.3, new Scalar(0, 255, 0));
                     }
 
-                    frameMat.PutText(string.Format("Blinks: {0}", TOTAL), new OpenCvSharp.Point(10, 30), HersheyFonts.HersheySimplex, 0.7, new Scalar(0, 0, 255), 2);
-
-                    Cv2.ImShow("img", frameMat);
-                    // Blink 3 times to save
-                    if (TOTAL == 3)
+                    var EAR1 = eyeAspectRatio(eye1);
+                    var EAR2 = eyeAspectRatio(eye2);
+                    // average the eye aspect ratio together for both eyes
+                    var ear = (EAR1 + EAR2) / 2.0;
+                    frameMat.PutText(string.Format("EAR: {0:N2}", ear), new OpenCvSharp.Point(300, 30), HersheyFonts.HersheySimplex, 0.7, new Scalar(0, 0, 255), 2);
+                    // check to see if the eye aspect ratio is below the blink
+                    // threshold, and if so, increment the blink frame counter
+                    if (ear < EYE_AR_THRESH)
                     {
-                        saveimg.ImWrite(string.Format("Resource/{0}.jpg", DateTime.Now.ToString("yyyyMMddHHmmss")));
-                        TOTAL = 0;
-                        img.Image = new Bitmap(saveimg.Cols, saveimg.Rows, (int)saveimg.Step(), System.Drawing.Imaging.PixelFormat.Format24bppRgb, saveimg.Data);
-                        isread = false;
-                        Cv2.DestroyAllWindows();
+                        COUNTER += 1;
+                        Debug.WriteLine(ear);
                     }
-
-                    Cv2.WaitKey(50);
+                    // otherwise, the eye aspect ratio is not below the blink
+                    // threshold
+                    else
+                    {
+                        //if the eyes were closed for a sufficient number of
+                        // then increment the total number of blinks
+                        if (COUNTER >= EYE_AR_CONSEC_FRAMES)
+                            TOTAL += 1;
+                        // reset the eye frame counter
+                        COUNTER = 0;
+                    }
                 }
+
+
+                frameMat.PutText(string.Format("Blinks: {0}", TOTAL), new OpenCvSharp.Point(10, 30), HersheyFonts.HersheySimplex, 0.7, new Scalar(0, 0, 255), 2);
+
+                Cv2.ImShow("img", frameMat);
+                // Blink 3 times to save
+                if (TOTAL == 3)
+                {
+                    saveimg.ImWrite(string.Format("Resource/{0}.jpg", DateTime.Now.ToString("yyyyMMddHHmmss")));
+                    TOTAL = 0;
+                    img.Image = new Bitmap(saveimg.Cols, saveimg.Rows, (int)saveimg.Step(), System.Drawing.Imaging.PixelFormat.Format24bppRgb, saveimg.Data);
+                    isread = false;
+                    Cv2.DestroyAllWindows();
+
+                    if (shape != null)
+                    {
+                        var faceChipDetail = Dlib.GetFaceChipDetails(shape, 150, 0.25);
+                        var faceChip = Dlib.ExtractImageChip<RgbPixel>(cimg, faceChipDetail);
+                        faces[0] = new Matrix<RgbPixel>(faceChip);
+                    }
+                }
+
+                Cv2.WaitKey(50);
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             isread = false;
             Cv2.DestroyAllWindows();
+        }
+        Matrix<RgbPixel>[] faces = new Matrix<RgbPixel>[2];
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            DialogResult result = fileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(fileDialog.FileName);
+                var img = Dlib.LoadImageAsMatrix<RgbPixel>(fileDialog.FileName);
+                var dets = detector.Operator(img);
+
+                foreach (var face in dets)
+                {
+                    var shape = sp.Detect(img, face);
+                    var faceChipDetail = Dlib.GetFaceChipDetails(shape, 150, 0.25);
+                    faces[1] = Dlib.ExtractImageChip<RgbPixel>(img, faceChipDetail);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (faces[0] != null && faces[1] != null)
+            {
+                var faceDescriptors = net.Operator(faces);
+                var diff = faceDescriptors[0] - faceDescriptors[1];
+                var desnum = Dlib.Length(diff);
+                //if (Dlib.Length(diff) < 0.6)
+                MessageBox.Show(String.Format("{0}({1:N2})", (desnum < 0.6).ToString(), desnum));
+            }
         }
     }
 }
