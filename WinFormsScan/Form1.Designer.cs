b934b3e57c8d7099758c1df82fdc7905cfb97846
LP <1570800510@qq.com>
2022-09-23 15:28:37 +0800

    People face detection

--- WinFormsScan/Form1.Designer.cs
+++ WinFormsScan/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -45,7 +47,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(38, 144);
+            this.button2.Location = new System.Drawing.Point(38, 111);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 29);
             this.button2.TabIndex = 1;
@@ -62,11 +64,33 @@
             this.textBox1.Size = new System.Drawing.Size(346, 450);
             this.textBox1.TabIndex = 2;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(38, 297);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "people";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(38, 368);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(521, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -82,5 +106,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- WinFormsScan/Form1.cs
+++ WinFormsScan/Form1.cs
@@ -1,5 +1,8 @@
 ﻿using OpenCvSharp;
 using System;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 namespace WinFormsScan
 {
@@ -43,6 +46,78 @@ namespace WinFormsScan
         {
             isread = false;
             Cv2.DestroyAllWindows();
+        }
+        bool save = false;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isread = true;
+
+            var face_detect = new CascadeClassifier("Resource/lbpcascade_frontalface_improved.xml");
+            using VideoCapture cap = new VideoCapture(0);
+            using var hog = new HOGDescriptor();
+            hog.SetSVMDetector(HOGDescriptor.GetDefaultPeopleDetector());
+            while (isread)
+            {
+                var frameMat = cap.RetrieveMat();
+                var image = frameMat.Clone();
+
+                var t1 = Task.Run(() =>
+                  {
+                      var peoples = hog.DetectMultiScale(image);
+                      frameMat.PutText(string.Format("Peoples: {0}", peoples.Length), new OpenCvSharp.Point(10, 30), HersheyFonts.HersheySimplex, 0.7, new Scalar(255, 0, 0), 2);
+                      foreach (var item in peoples)
+                      {
+                          frameMat.Rectangle(item, new Scalar(255, 0, 0));
+                      }
+                  });
+
+                var t2 = Task.Run(() =>
+                   {
+                       var faces = face_detect.DetectMultiScale(image);
+                       frameMat.PutText(string.Format("Faces: {0}", faces.Length), new OpenCvSharp.Point(300, 30), HersheyFonts.HersheySimplex, 0.7, new Scalar(0, 0, 255), 2);
+                       foreach (var item in faces)
+                       {
+                           frameMat.Rectangle(item, new Scalar(0, 0, 255));
+                       }
+                       if (save)
+                       {
+                           if (faces.Length > 0)
+                           {
+                               var strfilename = DateTime.Now.ToString("yyyyMMddHHmmss");
+                               if (!Directory.Exists("Resource/Faces"))
+                               {
+                                   Directory.CreateDirectory("Resource/Faces");
+                               }
+                               Cv2.ImWrite(string.Format("Resource/Faces/{0}.jpg", strfilename), image);
+
+                               for (int i = 0; i < faces.Length; i++)
+                               {
+                                   if (!Directory.Exists(string.Format("Resource/Faces/{0}", strfilename)))
+                                   {
+                                       Directory.CreateDirectory(string.Format("Resource/Faces/{0}", strfilename));
+                                   }
+                                   var face = new Mat(image, new Rect(faces[i].X, faces[i].Y, faces[i].Width, faces[i].Y));
+                                   Cv2.ImWrite(string.Format("Resource/Faces/{0}/face{1}.jpg", strfilename, i), face);
+                               }
+                               MessageBox.Show(string.Format("Save Success Path({0})", string.Format("Resource/Faces/{0}", strfilename)));
+                           }
+                           else
+                           {
+                               MessageBox.Show("Error:No face detected");
+                           }
+                           save = false;
+                       }
+                   });
+                Task.WaitAll(t1, t2);
+                Cv2.ImShow("img", frameMat);
+                Cv2.WaitKey(50);
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            save = true;
         }
     }
 }
