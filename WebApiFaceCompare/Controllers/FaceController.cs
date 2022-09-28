a38cff1c6b47fd6e41fb64e0c1c30bcd81553c7f
LP <1570800510@qq.com>
2022-09-28 09:51:36 +0800

    Memory error bug repair; Concurrent same memory results in

--- WebApiFaceCompare/Controllers/FaceController.cs
+++ WebApiFaceCompare/Controllers/FaceController.cs
@@ -21,6 +21,7 @@ namespace WebApiFaceCompare.Controllers
     [ApiController]
     public class FaceController : ControllerBase
     {
+
         /// <summary>
         /// TestCompare
         /// </summary>
@@ -29,9 +30,7 @@ namespace WebApiFaceCompare.Controllers
         {
             try
             {
-                var sp = ShapePredictor.Deserialize("Resource/shape_predictor_5_face_landmarks.dat");
-                var net = LossMetric.Deserialize("Resource/dlib_face_recognition_resnet_model_v1.dat");
-                var detector = Dlib.GetFrontalFaceDetector();
+                using LossMetric net = LossMetric.Deserialize("Resource/dlib_face_recognition_resnet_model_v1.dat");
                 var imgurl1 = new[] { "https://gitee.com/laolaolulu/public/raw/master/L1.jpeg", "https://gitee.com/laolaolulu/public/raw/master/thumb_A3.jpeg", "https://gitee.com/laolaolulu/public/raw/master/thumb_A7.jpeg", "https://gitee.com/laolaolulu/public/raw/master/thumb_L3.jpeg" };
                 var imgurl2 = new[] { "https://gitee.com/laolaolulu/public/raw/master/L2.jpeg", "https://gitee.com/laolaolulu/public/raw/master/thumb_A4.jpeg", "https://gitee.com/laolaolulu/public/raw/master/thumb_A8.jpeg", "https://gitee.com/laolaolulu/public/raw/master/thumb_L4.jpeg" };
                 for (int i = 0; i < imgurl1.Length; i++)
@@ -39,37 +38,33 @@ namespace WebApiFaceCompare.Controllers
                     Matrix<RgbPixel>[] faces = new Matrix<RgbPixel>[2];
                     var t1 = Task.Run(() =>
                     {
+                        using ShapePredictor sp = ShapePredictor.Deserialize("Resource/shape_predictor_5_face_landmarks.dat");
+                        using FrontalFaceDetector detector = Dlib.GetFrontalFaceDetector();
                         var req = WebRequest.CreateHttp(imgurl1[i]);
-                        using var ss1 = req.GetResponse().GetResponseStream();
-                        using var ss2 = (Bitmap)Image.FromStream(ss1);
-                        var img2 = ss2.ToMatrix<RgbPixel>();
-
-                        // Dlib.PyramidUp(img2);
-                        var s21 = img2.Clone().ToArray();
-                            var dets2 = detector.Operator(s21);
-                            foreach (var face in dets2)
-                            {
-                                var shape = sp.Detect(img2, face);
-                                var faceChipDetail = Dlib.GetFaceChipDetails(shape, 150, 0.25);
-                                faces[0] = Dlib.ExtractImageChip<RgbPixel>(img2, faceChipDetail);
-                            }
-                        
-
+                        using Matrix<RgbPixel> img2 = ((Bitmap)Image.FromStream(req.GetResponse().GetResponseStream())).ToMatrix<RgbPixel>();
+                        var dets2 = detector.Operator(img2);
+                        foreach (var face in dets2)
+                        {
+                            using var shape = sp.Detect(img2, face);
+                            using var faceChipDetail = Dlib.GetFaceChipDetails(shape, 150, 0.25);
+                            faces[0] = Dlib.ExtractImageChip<RgbPixel>(img2, faceChipDetail);
+                        }
                     });
 
                     var t2 = Task.Run(() =>
                     {
+                        using ShapePredictor sp = ShapePredictor.Deserialize("Resource/shape_predictor_5_face_landmarks.dat");
+                        using FrontalFaceDetector detector = Dlib.GetFrontalFaceDetector();
                         var req1 = WebRequest.CreateHttp(imgurl2[i]);
                         using var stre = req1.GetResponse().GetResponseStream();
                         using var stre2 = (Bitmap)Image.FromStream(stre);
-                        using (var img21 = stre2.ToMatrix<RgbPixel>())
+                        using (Matrix<RgbPixel> img21 = stre2.ToMatrix<RgbPixel>())
                         {
-                            //Dlib.PyramidUp(img21);
-                            var dets21 = detector.Operator(img21.Clone());
+                            var dets21 = detector.Operator(img21);
                             foreach (var face1 in dets21)
                             {
-                                var shape1 = sp.Detect(img21, face1);
-                                var faceChipDetail1 = Dlib.GetFaceChipDetails(shape1, 150, 0.25);
+                                using var shape1 = sp.Detect(img21, face1);
+                                using var faceChipDetail1 = Dlib.GetFaceChipDetails(shape1, 150, 0.25);
                                 faces[1] = Dlib.ExtractImageChip<RgbPixel>(img21, faceChipDetail1);
                             }
                         }
@@ -108,20 +103,19 @@ namespace WebApiFaceCompare.Controllers
         [HttpPost]
         public string Compare(IFormFile image, string imgUrl = "https://gitee.com/laolaolulu/public/raw/master/20220920192624.jpg")
         {
-            using var sp = ShapePredictor.Deserialize("Resource/shape_predictor_5_face_landmarks.dat");
             using var net = LossMetric.Deserialize("Resource/dlib_face_recognition_resnet_model_v1.dat");
-            using var detector = Dlib.GetFrontalFaceDetector();
-
             Matrix<RgbPixel>[] faces = new Matrix<RgbPixel>[2];
 
             var t1 = Task.Run(() =>
             {
                 using var img1 = ((Bitmap)Image.FromStream(image.OpenReadStream())).ToMatrix<RgbPixel>();
+                using var sp = ShapePredictor.Deserialize("Resource/shape_predictor_5_face_landmarks.dat");
+                using var detector = Dlib.GetFrontalFaceDetector();
                 var dets1 = detector.Operator(img1);
                 foreach (var face in dets1)
                 {
-                    var shape = sp.Detect(img1, face);
-                    var faceChipDetail = Dlib.GetFaceChipDetails(shape, 150, 0.25);
+                    using var shape = sp.Detect(img1, face);
+                    using var faceChipDetail = Dlib.GetFaceChipDetails(shape, 150, 0.25);
                     faces[0] = Dlib.ExtractImageChip<RgbPixel>(img1, faceChipDetail);
                 }
             });
@@ -130,11 +124,13 @@ namespace WebApiFaceCompare.Controllers
             {
                 var req = WebRequest.CreateHttp(imgUrl);
                 using var img2 = ((Bitmap)Image.FromStream(req.GetResponse().GetResponseStream())).ToMatrix<RgbPixel>();
+                using var sp = ShapePredictor.Deserialize("Resource/shape_predictor_5_face_landmarks.dat");
+                using var detector = Dlib.GetFrontalFaceDetector();
                 var dets2 = detector.Operator(img2);
                 foreach (var face in dets2)
                 {
-                    var shape = sp.Detect(img2, face);
-                    var faceChipDetail = Dlib.GetFaceChipDetails(shape, 150, 0.25);
+                    using var shape = sp.Detect(img2, face);
+                    using var faceChipDetail = Dlib.GetFaceChipDetails(shape, 150, 0.25);
                     faces[1] = Dlib.ExtractImageChip<RgbPixel>(img2, faceChipDetail);
                 }
             });
@@ -144,8 +140,8 @@ namespace WebApiFaceCompare.Controllers
 
             if (faces[0] != null && faces[1] != null)
             {
-                var faceDescriptors = net.Operator(faces);
-                var diff = faceDescriptors[0] - faceDescriptors[1];
+                using var faceDescriptors = net.Operator(faces);
+                using var diff = faceDescriptors[0] - faceDescriptors[1];
                 var desnum = Dlib.Length(diff);
                 return string.Format("{0}({1:N2})", (desnum < 0.5).ToString(), desnum);
             }
